b47c14a33969a3373a1b036d5ef0ce79cdd924df
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-08 12:35:30 -0600

    graph

--- client/src/reducers/coins.js
+++ client/src/reducers/coins.js
@@ -5,35 +5,38 @@ const ADD_COIN = 'ADD_COIN'
 const REMOVE_COIN = 'REMOVE_COIN'
 
 export const getCoins = () => {
-  return(dispatch) => {
+  return (dispatch) => {
     axios.get('/api/coins')
-      .then( res => dispatch({ type: COINS, coins: res.data}) )
+      .then( res => dispatch({ type: COINS, coins: res.data }) )
   }
 }
 
-export const addCoins = (coin) => {
+export const addCoin = (coin) => {
   return (dispatch) => {
     axios.post('/api/coins', { coin })
-      .then ( res => dispatch({ type: ADD_COIN, coin: res.data}))
+      .then( res => dispatch({ type: ADD_COIN, coin: res.data }) )
   }
 }
 
-export const removeCoins = (id) => {
+export const removeCoin = (id) => {
   return (dispatch) => {
     axios.put(`/api/coins/${id}`)
-      .then ( () => dispatch({ tyoe: REMOVE_COIN, id}))
+      .then( () => dispatch({ type: REMOVE_COIN, id }) )
   }
 }
 
-export default ( state = [], action) => {
+export default ( state = [], action ) => {
   switch(action.type) {
     case COINS:
+      // { type: COINS, coins: [{},{}] }
       return action.coins
     case ADD_COIN:
+      // { type: ADD_COIN, coin: {} }
       return [...state, action.coin]
     case REMOVE_COIN:
-    return state.filter( c => c.id !== action.id)
-    default:
-     return state
+      // { type: REMOVE_COIN, id: 7 }
+      return state.filter( c => c.id !== action.id )
+    default: 
+      return state
   }
 }
