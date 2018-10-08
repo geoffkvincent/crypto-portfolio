e73d43f6d3a2e6d5de3e67a9c3f267191506bc74
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-08 11:35:48 -0600

    coin list component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import CoinForm from '/CoinForm'
-import CoinList from '/CoinList'
+import CoinForm from './CoinForm'
+import CoinList from './CoinList'
 
 
 const Home = () => (
