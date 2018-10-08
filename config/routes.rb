0c841b18d8616e54869c97cd627efb3254339cbb
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-08 10:14:09 -0600

    controllers and models setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'coins/index'
-    get 'coins/create'
-    get 'coins/show'
-    get 'coins/update'
-  end
+  
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    resources :coins
   end
 
   #Do not place any routes below this one
