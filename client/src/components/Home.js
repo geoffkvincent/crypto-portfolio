bb3ffebfe3e0ec437b8e00945f8a5131b11af968
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-08 10:23:27 -0600

    coin crud actions

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
+import axios from 'axios'
 
 class Home extends Component {
+  state = {coin: {}}
+
+  clicky=() =>{
+    axios.post('/api/coins?coin=btc')
+    .then(res => {debugger})
+  }
+
+  click2=() =>{
+    axios.post('/api/coins')
+    .then(res => {debugger})
+  }
+
   render() {
     return (
-      <Header as="h1" textAlign="center">Home Component</Header>
+      <>
+      <Button onClick={this.clicky}>
+        Click Me
+      </Button>
+      <Button onClick={this.clicky2}>
+       Coins
+      </Button>
+      </>
     );
   }
 }
