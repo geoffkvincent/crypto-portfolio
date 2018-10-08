f2238dab1016964d2e0cc745285f63d2178f756c
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-08 14:24:41 -0600

    rest from fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,21 @@ import React from 'react';
 import CoinForm from './CoinForm'
 import CoinList from './CoinList'
 
+class Home extends React.Component {
+  state = { key: 0 }
 
-const Home = () => (
-  <>
-    <CoinForm />
-    <CoinList />
-  </>
-)
+  resetForm = () => {
+    this.setState( state => ({ key: state.key + 1 }) )
+  }
 
+  render() {
+    return (
+      <>
+        <CoinForm key={this.state.key} resetForm={this.resetForm} />
+        <CoinList />
+      </>
+    )
+  }
+}
 
 export default Home;
-
